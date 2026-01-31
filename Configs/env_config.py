d89b752752c9363f7d468d56f1aa4aab0b468a4d
Kara <alexkartsev950@gmail.com>
2026-01-31 20:56:38 +0300

    modified:   Configs/env_config.py 	modified:   Configs/test_data.py

--- Configs/env_config.py
+++ Configs/env_config.py
@@ -1,8 +1,8 @@
 # переменные окружения (URL, токены, логины)
 BASE_URL = "https://www.kinopoisk.ru/"
 API_ENDPOINT = "https://api.poiskkino.dev/"
-USERNAME = "gexali1522@1200b.com"
-PASSWORD = "test123"
+# USERNAME = "gexali1522@1200b.com"
+# PASSWORD = "test123"
 TOKEN = "T2G0VMW-KZ54KST-GVCXD7P-BK6XRE4"
 # поиск по названию
 Url_serch = f"{API_ENDPOINT}/v1.4/movie/search"
